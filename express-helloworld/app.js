d7c3b1fd728f59a88c4ddd75af420cac7c8f70b1
thevivekyadav <vivekyadavclasher@gmail.com>
2024-07-28 21:21:05 +0530

    added more routes

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -8,6 +8,19 @@ app.get('/', function (req, res) {
 app.get('/twitter', (req, res)=>{
   res.send('Twitter\n');
 })
+app.get('/login', (req, res) => {
+    res.send('<h1>Please Login at chai aur code </h1>');
+  });
+  
+  app.get('/myData',(req, res)=>{
+    res.json({name:"vivek",
+      age:"12"
+    })
+  })
+  
+  app.get('/vivek', (req, res) => {
+    res.send('<h2>Chai Aur Code</h2>');
+  });
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
